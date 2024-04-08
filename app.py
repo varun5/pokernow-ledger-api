fc91b8955c15309682962daa39595a1d1971a41d
Varun Nayak <varun.d.nayak@gmail.com>
2024-04-07 20:39:55 -0400

    CORS fix

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 def minimize_transactions(ledger):
     positives = [(name, net) for name, net in ledger.items() if net > 0]
     negatives = [(name, -net) for name, net in ledger.items() if net < 0]
